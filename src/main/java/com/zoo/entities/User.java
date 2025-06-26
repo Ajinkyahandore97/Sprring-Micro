1225bf1d2d72dc7dde853c785a4697db7eb2f416
Ajinkyahandore97 <handoreajinkya21@gmail.com>
2025-06-26 13:14:44 +0530

    create new file and modify user entity class

--- src/main/java/com/zoo/entities/User.java
+++ src/main/java/com/zoo/entities/User.java
@@ -1,13 +1,12 @@
 package com.zoo.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table(name = "Users")
@@ -29,4 +28,6 @@ public class User {
     @Column(name = "ABOUT")
     private String about;
 
+    @Transient
+    List<Ratings> ratings;
 }
